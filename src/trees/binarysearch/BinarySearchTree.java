21413a25ba832a9fd933e96a6413734dccedb8bd
JoaoRobert0 <devjoaoroberto@gmail.com>
2024-10-16 10:25:28 -0300

    First scratch binary search tree

--- src/trees/binarysearch/BinarySearchTree.java
+++ src/trees/binarysearch/BinarySearchTree.java
@@ -2,4 +2,26 @@ package trees.binarysearch;
 
 public class BinarySearchTree {
     
+    private Node root;
+    private int size = 0;
+
+    public BinarySearchTree() {
+        this.root = null;
+    }
+
+    public boolean isEmpty() {
+        if (this.root == null) return true;
+        return false;
+    }
+
+    public void insert(int key) {
+        insertRecursive(this.root, key);
+        size++;
+    }
+
+    private void insertRecursive(Node root, int key) {
+        
+    } 
+
+
 }
