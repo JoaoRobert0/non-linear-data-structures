633ebbffac757875eed551328db43da5b975b0f3
João Roberto Chaves <141665911+JoaoRobert0@users.noreply.github.com>
2024-10-27 20:40:50 +0000

    Create the class TestBinarySearchTree using the codespace ;)

---
+++ tests/trees/binarysearch/TestBinarySearchTree.java
@@ -0,0 +1,12 @@
+package tests.trees.binarysearch;
+
+import trees.binarysearch.*;
+
+public class TestBinarySearchTree {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        System.out.println(bst.isEmpty());
+
+    }
+}
